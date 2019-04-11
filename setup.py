effdd061d8ad8c30f5d181f1716e78d28b7c68a5
Guillaume Vanel <guillaume.vanel@insa-lyon.fr>
2019-04-11 08:44:52 +0200

    package

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Projet-deploiement",
+    version="0.0.1",
+    author="Guillaume VANEL",
+    author_email="guillaume.vanel@insa-lyon.fr",
+    description="Un projet de création d'un package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/draguar/ProjetSergio",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
